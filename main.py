0d9207039089dfdfbc56270be991e998517aa8cd
Falcone8 <maule2703@gmail.com>
2022-01-03 18:49:15 +0000

    proof

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from telethon.sessions import StringSession
 from telethon import TelegramClient
 
 
-api_id = 12138400
-api_hash = 'e4d75abe5ed054bf7139f1251d9e2163'
-client = TelegramClient(StringSession('1BJWap1wBu12qc71JFiIffIC3Wp7sSMmoLoQVomSvk1uO8M4aHtn1cLIX7CLJaMT1ZfoVbdsqHzOOvv1fts-nOWExcD5CXhBMk2OnGWcrCMuF2J3tbXG0ZEdTYL6JmoZC8yeZsyZtBqNs_TydZZSLSGUqvZihoS0aEnvNUv9b4yelIGqGFZ904SK1U0bhODcHyjtOxVtggqg3M67kvAV-4fubO07w3jkiH17lreaUJChH5C1gWXtGw5hVdeupqXP0n9qlIZIuOM1U0Rnop_-rckN0gKQAkRp1Hc1rWGO8MHXrPdrD4vAM7KMXKSma83WuJcqVIsNFehz_5o7nE9Rb9PR1KpflZBI='), api_id, api_hash)
-string = '1BJWap1wBu12qc71JFiIffIC3Wp7sSMmoLoQVomSvk1uO8M4aHtn1cLIX7CLJaMT1ZfoVbdsqHzOOvv1fts-nOWExcD5CXhBMk2OnGWcrCMuF2J3tbXG0ZEdTYL6JmoZC8yeZsyZtBqNs_TydZZSLSGUqvZihoS0aEnvNUv9b4yelIGqGFZ904SK1U0bhODcHyjtOxVtggqg3M67kvAV-4fubO07w3jkiH17lreaUJChH5C1gWXtGw5hVdeupqXP0n9qlIZIuOM1U0Rnop_-rckN0gKQAkRp1Hc1rWGO8MHXrPdrD4vAM7KMXKSma83WuJcqVIsNFehz_5o7nE9Rb9PR1KpflZBI='
+api_id = 
+api_hash = ''
+client = TelegramClient(StringSession(''), api_id, api_hash)
+string = ''
 print("Avvio userbot...")
 client.start()
 print("Userbot avviato!")
