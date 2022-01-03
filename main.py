82b1adc3889ca42a57a967d4190c1d6696108a98
Falcone8 <maule2703@gmail.com>
2022-01-03 14:40:29 +0000

    Rewrite the bot in telethon

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from telethon import TelegramClient
 
 api_id = 12138400
 api_hash = 'e4d75abe5ed054bf7139f1251d9e2163'
-client = TelegramClient(StringSession(string), api_id, api_hash)
-
+client = TelegramClient(StringSession('1BJWap1wBu12qc71JFiIffIC3Wp7sSMmoLoQVomSvk1uO8M4aHtn1cLIX7CLJaMT1ZfoVbdsqHzOOvv1fts-nOWExcD5CXhBMk2OnGWcrCMuF2J3tbXG0ZEdTYL6JmoZC8yeZsyZtBqNs_TydZZSLSGUqvZihoS0aEnvNUv9b4yelIGqGFZ904SK1U0bhODcHyjtOxVtggqg3M67kvAV-4fubO07w3jkiH17lreaUJChH5C1gWXtGw5hVdeupqXP0n9qlIZIuOM1U0Rnop_-rckN0gKQAkRp1Hc1rWGO8MHXrPdrD4vAM7KMXKSma83WuJcqVIsNFehz_5o7nE9Rb9PR1KpflZBI='), api_id, api_hash)
+string = '1BJWap1wBu12qc71JFiIffIC3Wp7sSMmoLoQVomSvk1uO8M4aHtn1cLIX7CLJaMT1ZfoVbdsqHzOOvv1fts-nOWExcD5CXhBMk2OnGWcrCMuF2J3tbXG0ZEdTYL6JmoZC8yeZsyZtBqNs_TydZZSLSGUqvZihoS0aEnvNUv9b4yelIGqGFZ904SK1U0bhODcHyjtOxVtggqg3M67kvAV-4fubO07w3jkiH17lreaUJChH5C1gWXtGw5hVdeupqXP0n9qlIZIuOM1U0Rnop_-rckN0gKQAkRp1Hc1rWGO8MHXrPdrD4vAM7KMXKSma83WuJcqVIsNFehz_5o7nE9Rb9PR1KpflZBI='
 print("Avvio userbot...")
 client.start()
 print("Userbot avviato!")
@@ -22,4 +22,4 @@ async def info(event):
              elif x.sender.bot == False:
                 is_bot = "✖"
              await event.edit(f"INFORMAZIONI SULL'UTENTE\nNome: {x.sender.first_name}\nID: » <code>{x.sender.id}</code>\nBot: {is_bot}\nUsername: {x.sender.username}\nDC: {x.sender.photo.dc_id}",parse_mode="html")
-             
+             lol
