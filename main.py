1b8c1799205374fe96e0213eac715a206e347077
Falcone8 <maule2703@gmail.com>
2022-01-04 17:35:36 +0000

    Commit

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from pyrogram import Client
 
 app = Client("my_account", config_file="my_configuration.ini")
+
+@app.on_message(filters.command("info", "."))
+async def info(Client, message):
+
+    user = await app.get_users( message.text.split(" ")[1])
+
+    print(user)
+    await app.send_message(message.chat.id, f"info di {user.first_name}\n\nid: {user.id}")
+
+app.run()
